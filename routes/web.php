16bbd8a36be8172381daedf49a92630e373073ee
Lalhmangaihenga <valpuia604@gmail.com>
2023-06-28 15:25:04 +0700

    checking data

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Squire\Models\Airline;
+use Squire\Models\Airport;
+use Squire\Models\Continent;
+use Squire\Models\Country;
+use Squire\Models\Currency;
+use Squire\Models\GbCounty;
+use Squire\Models\Region;
+use Squire\Models\Timezone;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +23,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+    // dd(
+    //     // Airline::with(['country', 'continent'])->first()
+    //     // Airport::with(['country', 'timezone'])->first()
+    //     // Continent::with(['timezones'])->first()
+    //     // Country::with(['airlines', 'airports', 'continent', 'currency', 'regions', 'timezones'])->first()
+    //     // Currency::with('countries')->first()
+    //     // GbCounty::with('region')->first()
+    //     // Region::with('gbCounties')->first()
+    //     // Timezone::where('code', 'like', 'Asia/%')->get()
+    // );
 });
